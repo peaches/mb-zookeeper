b963a9f7d09222c4adbec20934a04409b797eacd
Topping Bowers <topper@host73.dyn23.corp.mbox>
2010-05-18 18:14:10 -0400

    improve the eventhandler

--- lib/zookeeper/event_handler.rb
+++ lib/zookeeper/event_handler.rb
@@ -2,6 +2,8 @@ class ZooKeeper
   class EventHandler
     import org.apache.zookeeper.Watcher if defined?(JRUBY_VERSION)
 
+    attr_accessor :zk
+
     def initialize(zookeeper_client)
       @zk = zookeeper_client
       @callbacks = {}
@@ -10,7 +12,7 @@ class ZooKeeper
     def handle_process(event)
       if event.path and @callbacks[event.path]
         @callbacks[event.path].each do |callback|
-          callback.call(event)
+          callback.call(event, @zk) if callback.respond_to?(:call)
         end
       end
     end
@@ -18,6 +20,11 @@ class ZooKeeper
     def register(path, &block)
       @callbacks[path] ||= []
       @callbacks[path] << block
+      return @callbacks[path].index(block)
+    end
+
+    def unregister(path, index)
+      @callbacks[path][index] = nil
     end
 
     if defined?(JRUBY_VERSION)
