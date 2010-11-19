ee81751f2702ac1776883040f469d7cd113b3995
Topper Bowers <topper@toppingdesign.com>
2010-11-19 13:11:17 -0500

    blocking lock

--- lib/zookeeper/locker.rb
+++ lib/zookeeper/locker.rb
@@ -7,43 +7,74 @@ class ZooKeeper
       @zk = zookeeper_client
       @root_lock_node = root_lock_node
       @path = path
-      if !@zk.exists?(root_lock_node)
-        @zk.create(root_lock_node, "", :mode => :persistent)
+      @zk.create(root_lock_node, "", :mode => :persistent) rescue KeeperException::NodeExists
+    end
+
+    #blocking lock
+    def with_lock(&blk)
+      create_lock_file!
+      queue = Queue.new
+
+      first_lock_blk = lambda do
+        if have_first_lock?(true)
+           queue << :locked
+         end
       end
+
+      @zk.watcher.register(root_lock_path, &first_lock_blk)
+      first_lock_blk.call
+
+      if queue.pop
+        begin
+          @locked = true
+          return blk.call
+        ensure
+          unlock!
+        end
+      end
+
     end
 
     def lock!
-      path = lock_path(@path)
-      @zk.create(path, "", :mode => :persistent) rescue KeeperException::NodeExists
-      @lock_file = @zk.create("#{path}/lock", "", :mode => :ephemeral_sequential)
-      lock_files = @zk.children(path)
-      lock_files.sort! {|a,b| digit_from_lock_file(a) <=> digit_from_lock_file(b)}
-      if digit_from_lock_file(lock_files.first) == digit_from_lock_file(@lock_file)
+      create_lock_file!
+      if have_first_lock?(false)
         @locked = true
-        return true
       else
-        @zk.delete(@lock_file)
-        return false
+        cleanup_lock_file!
+        false
       end
     end
 
     def unlock!
       if @locked
-        @zk.delete(@lock_file)
-        if @zk.children(lock_path(@path)).empty?
-          begin
-            @zk.delete(lock_path(@path))
-          rescue
-            nil
-          end
-        end
+        cleanup_lock_file!
         @locked = false
-        return true
+        true
       end
     end
 
-    def lock_path(path)
-      "#{@root_lock_node}/#{path.gsub("/", "__")}"
+private
+
+    def cleanup_lock_file!
+      @zk.delete(@lock_file)
+      @zk.delete(root_lock_path) rescue KeeperException::NotEmpty
+    end
+
+    def have_first_lock?(watch = true)
+      lock_files = @zk.children(root_lock_path, :watch => watch)
+      lock_files.sort! {|a,b| digit_from_lock_file(a) <=> digit_from_lock_file(b)}
+      digit_from_lock_file(lock_files.first) == digit_from_lock_file(@lock_file)
+    end
+
+    def create_lock_file!
+      @zk.create(root_lock_path, "", :mode => :persistent) rescue KeeperException::NodeExists
+      @lock_file = @zk.create("#{root_lock_path}/lock", "", :mode => :ephemeral_sequential)
+    rescue KeeperException::NoNode
+      retry
+    end
+
+    def root_lock_path
+      "#{@root_lock_node}/#{@path.gsub("/", "__")}"
     end
 
     def digit_from_lock_file(lock_path)
--- spec/locker_spec.rb
+++ spec/locker_spec.rb
@@ -3,8 +3,8 @@ require File.join(File.dirname(__FILE__), %w[spec_helper])
 describe ZooKeeper::Locker do
 
   before(:each) do
-    @zk = ZooKeeper.new("localhost:2181")
-    @zk2 = ZooKeeper.new("localhost:2181")
+    @zk = ZooKeeper.new("localhost:2181", :watcher => :default)
+    @zk2 = ZooKeeper.new("localhost:2181", :watcher => :default)
     wait_until{ @zk.connected? && @zk2.connected? }
     @path_to_lock = "/lock_tester"
   end
@@ -46,6 +46,25 @@ describe ZooKeeper::Locker do
 
   it "should be able to handle multi part path locks" do
     @zk.locker("my/multi/part/path").lock!.should be_true
+  end
+
+  describe "blocking lock" do
+    it "should blocking lock" do
+      first_lock = @zk.locker("mylock")
+      first_lock.lock!.should be_true
+      second_lock_called = false
+      thread = Thread.new do
+        @zk.locker("mylock").with_lock do
+          second_lock_called = true
+        end
+      end
+      second_lock_called.should be_false
+      first_lock.unlock!
+      thread.join(10)
+      second_lock_called.should be_true
+    end
+
+
   end
   
 
